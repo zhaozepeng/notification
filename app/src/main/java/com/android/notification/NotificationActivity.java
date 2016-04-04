69a99741d990547a774aac6b0dade6e827d747ff
zhao_zepeng <zhao_zepeng@hotmail.com>
2016-04-04 20:44:57 +0800

    notification完成100%

--- app/src/main/java/com/android/notification/NotificationActivity.java
+++ app/src/main/java/com/android/notification/NotificationActivity.java
@@ -5,8 +5,11 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.NotificationCompat;
 import android.view.View;
 import android.widget.Button;
@@ -17,6 +20,7 @@ import com.android.libcore.log.L;
 import com.android.libcore_ui.activity.BaseActivity;
 
 import java.text.SimpleDateFormat;
+import java.util.List;
 import java.util.Locale;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -45,6 +49,11 @@ public class NotificationActivity extends BaseActivity implements View.OnClickLi
     private Timer timer;
     private TimerTask task;
     private Button btn_dismiss_dynamic;
+    private Button btn_float;
+    private Button btn_Heads_up;
+    private Button btn_start_activity;
+    private Button btn_start_activity_task;
+    private Button btn_start_single_activity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,6 +73,16 @@ public class NotificationActivity extends BaseActivity implements View.OnClickLi
         btn_big.setOnClickListener(this);
         btn_dismiss_dynamic = (Button) findViewById(R.id.btn_dismiss_dynamic);
         btn_dismiss_dynamic.setOnClickListener(this);
+        btn_float = (Button) findViewById(R.id.btn_Heads_up);
+        btn_float.setOnClickListener(this);
+        btn_Heads_up = (Button) findViewById(R.id.btn_Heads_up);
+        btn_Heads_up.setOnClickListener(this);
+        btn_start_activity = (Button) findViewById(R.id.btn_start_activity_task);
+        btn_start_activity.setOnClickListener(this);
+        btn_start_activity_task = (Button) findViewById(R.id.btn_start_activity_task);
+        btn_start_activity_task.setOnClickListener(this);
+        btn_start_single_activity = (Button) findViewById(R.id.btn_start_single_activity);
+        btn_start_single_activity.setOnClickListener(this);
     }
 
     private void initData() {
@@ -79,7 +98,8 @@ public class NotificationActivity extends BaseActivity implements View.OnClickLi
         super.onReceive(context, intent);
         if (intent.getAction().equals(NOTIFY_ACTION)) {
             T.getInstance().showShort("you click notification");
-        }else{
+        } else {
+            T.getInstance().showShort("you close notification!!");
             notificationManager.cancel(NOTIFY_ID3);
         }
     }
@@ -97,8 +117,8 @@ public class NotificationActivity extends BaseActivity implements View.OnClickLi
                 mBuilder.setSmallIcon(R.mipmap.ic_launcher)
                         .setContentTitle("this is notification title test")
                         .setContentText("this is notification text test")
-                        .setNumber((int) (Math.random() *1000))
-                .setTicker("you got a new message")
+                        .setNumber((int) (Math.random() * 1000))
+                        .setTicker("you got a new message")
                         .setDefaults(Notification.DEFAULT_SOUND
                                 | Notification.DEFAULT_VIBRATE | Notification.DEFAULT_LIGHTS)
                         .setAutoCancel(true)
@@ -109,32 +129,45 @@ public class NotificationActivity extends BaseActivity implements View.OnClickLi
                 mBuilder.setContentIntent(pendingIntent);
                 notification = mBuilder.build();
                 notificationManager.notify(NOTIFY_ID1, notification);
-            break;
+                break;
             case R.id.btn_show_dynamic:
                 startTimeTask();
                 break;
             case R.id.btn_big:
+                RemoteViews smallView = new RemoteViews(getPackageName(), R.layout.layout_notification);
+                smallView.setTextViewText(R.id.tv_number, parseDate());
+                smallView.setImageViewResource(R.id.iv_icon, R.mipmap.ic_launcher);
+
                 mBuilder = new NotificationCompat.Builder(NotificationActivity.this);
                 mBuilder.setSmallIcon(R.mipmap.ic_launcher)
-                        .setContentTitle("this is notification title test")
-                        .setContentText("this is notification text test")
-                        .setNumber((int) (Math.random() *1000))
+                        .setNumber((int) (Math.random() * 1000))
+                                //No longer displayed in the status bar as of API 21.
                         .setTicker("you got a new message")
                         .setDefaults(Notification.DEFAULT_SOUND
                                 | Notification.DEFAULT_VIBRATE | Notification.DEFAULT_LIGHTS)
                         .setAutoCancel(true)
-                        .setWhen(0);
+                        .setWhen(0)
+                        .setPriority(NotificationCompat.PRIORITY_LOW);
                 intent = new Intent(NOTIFY_ACTION);
                 pendingIntent = PendingIntent.getBroadcast(NotificationActivity.this,
                         1000, intent, PendingIntent.FLAG_UPDATE_CURRENT);
                 mBuilder.setContentIntent(pendingIntent);
-                notification = mBuilder.build();
 
+                //在5.0版本之后，可以支持在锁屏界面显示notification
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    mBuilder.setVisibility(NotificationCompat.VISIBILITY_PUBLIC);
+                }
+                notification = mBuilder.build();
+                notification.contentView = smallView;
+
+                //如果系统版本 >= Android 4.1，设置大视图 RemoteViews
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
                     RemoteViews view = new RemoteViews(getPackageName(), R.layout.layout_big_notification);
+                    view.setTextViewText(R.id.tv_name, "我是名字1我是名字2我是名字3我是名字4我是名字5我是名字6我是名字7我是名字");
                     view.setOnClickPendingIntent(R.id.btn_click_close,
                             PendingIntent.getBroadcast(NotificationActivity.this, 1001,
-                                    new Intent(), PendingIntent.FLAG_UPDATE_CURRENT));
+                                    new Intent(CLICK_ACTION), PendingIntent.FLAG_UPDATE_CURRENT));
+                    //textview marquee property is useless for bigContentView
                     notification.bigContentView = view;
                 }
 
@@ -143,10 +176,89 @@ public class NotificationActivity extends BaseActivity implements View.OnClickLi
             case R.id.btn_dismiss_dynamic:
                 if (notification != null) {
                     notificationManager.cancel(NOTIFY_ID2);
-                    timer.cancel();
-                    timer = null;
-                    task = null;
+                    if (timer != null) {
+                        timer.cancel();
+                        timer = null;
+                        task = null;
+                    }
                 }
+                break;
+            case R.id.btn_Heads_up:
+                RemoteViews headsUpView = new RemoteViews(getPackageName(), R.layout.layout_heads_up_notification);
+
+                intent = new Intent(NOTIFY_ACTION);
+                pendingIntent = PendingIntent.getBroadcast(NotificationActivity.this,
+                        1000, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+                mBuilder = new NotificationCompat.Builder(NotificationActivity.this);
+                mBuilder.setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentTitle("this is notification title test")
+                        .setContentText("this is notification text test")
+                        .setNumber((int) (Math.random() * 1000))
+                        .setTicker("you got a new message")
+                                //must set pendingintent for this notification, or will be crash
+                        .setContentIntent(pendingIntent)
+                        .setDefaults(Notification.DEFAULT_SOUND
+                                | Notification.DEFAULT_VIBRATE | Notification.DEFAULT_LIGHTS)
+                        .setAutoCancel(true)
+                        .setWhen(0);
+                notification = mBuilder.build();
+                if (Build.VERSION.SDK_INT >= 21) {
+                    notification.priority = Notification.PRIORITY_MAX;
+                    notification.headsUpContentView = headsUpView;
+                }
+                notificationManager.notify(NOTIFY_ID1, notification);
+                break;
+            case R.id.btn_start_activity_task:
+                mBuilder = new NotificationCompat.Builder(NotificationActivity.this);
+                mBuilder.setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentTitle("this is notification title test")
+                        .setContentText("this is notification text test")
+                        .setNumber((int) (Math.random() * 1000))
+                        .setTicker("you got a new message")
+                        .setDefaults(Notification.DEFAULT_SOUND
+                                | Notification.DEFAULT_VIBRATE | Notification.DEFAULT_LIGHTS)
+                        .setAutoCancel(true)
+                        .setWhen(0);
+
+                intent = new Intent(this, ChildActivity.class);
+                TaskStackBuilder taskStackBuilder = TaskStackBuilder.create(this);
+                taskStackBuilder.addParentStack(ChildActivity.class);
+                taskStackBuilder.addNextIntent(intent);
+                PendingIntent resultPendingIntent =
+                        taskStackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+                mBuilder.setContentIntent(resultPendingIntent);
+                notification = mBuilder.build();
+                notificationManager.notify(NOTIFY_ID1, notification);
+                break;
+            case R.id.btn_start_single_activity:
+
+                mBuilder = new NotificationCompat.Builder(NotificationActivity.this);
+                mBuilder.setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentTitle("this is notification title test")
+                        .setContentText("this is notification text test")
+                        .setNumber((int) (Math.random() * 1000))
+                        .setTicker("you got a new message")
+                        .setDefaults(Notification.DEFAULT_SOUND
+                                | Notification.DEFAULT_VIBRATE | Notification.DEFAULT_LIGHTS)
+                        .setAutoCancel(true)
+                        .setWhen(0);
+
+                intent = new Intent(this, SingleActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                        | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                PendingIntent notifyPendingIntent =
+                        PendingIntent.getActivity(
+                                this,
+                                0,
+                                intent,
+                                PendingIntent.FLAG_UPDATE_CURRENT
+                        );
+
+                mBuilder.setContentIntent(notifyPendingIntent);
+                notification = mBuilder.build();
+                notificationManager.notify(NOTIFY_ID1, notification);
+                setBadge(this, 3);
                 break;
         }
     }
@@ -177,11 +289,46 @@ public class NotificationActivity extends BaseActivity implements View.OnClickLi
 
         mBuilder.setSmallIcon(R.mipmap.ic_launcher)
                 .setContentIntent(pendingIntent)
-                .setContentTitle("this is notification title test")
                 .setTicker("you got a new message")
                 .setOngoing(true)
                 .setContent(view);
         notification = mBuilder.build();
         notificationManager.notify(NOTIFY_ID2, notification);
+    }
+
+    public static void setBadge(Context context, int count) {
+        String launcherClassName = getLauncherClassName(context);
+        if (launcherClassName == null) {
+            return;
+        }
+        Intent intent = new Intent("android.intent.action.BADGE_COUNT_UPDATE");
+        intent.putExtra("badge_count", count);
+        intent.putExtra("badge_count_package_name", context.getPackageName());
+        intent.putExtra("badge_count_class_name", launcherClassName);
+        context.sendBroadcast(intent);
+    }
+
+    public static String getLauncherClassName(Context context) {
+
+        PackageManager pm = context.getPackageManager();
+
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        List<ResolveInfo> resolveInfos = pm.queryIntentActivities(intent, 0);
+        for (ResolveInfo resolveInfo : resolveInfos) {
+            String pkgName = resolveInfo.activityInfo.applicationInfo.packageName;
+            if (pkgName.equalsIgnoreCase(context.getPackageName())) {
+                String className = resolveInfo.activityInfo.name;
+                return className;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        L.e("onDestroy");
     }
 }
---
+++ app/src/main/java/com/android/notification/ParentTwoActivity.java
@@ -0,0 +1,36 @@
+package com.android.notification;
+
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.android.libcore.log.L;
+import com.android.libcore_ui.activity.BaseActivity;
+
+/**
+ * Description: #TODO
+ *
+ * @author zzp(zhao_zepeng@hotmail.com)
+ * @since 2016-04-04
+ */
+public class ParentTwoActivity extends BaseActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        useToolbar = false;
+        super.onCreate(savedInstanceState);
+        TextView textView = new TextView(this);
+        textView.setBackgroundColor(ContextCompat.getColor(this, R.color.white));
+        textView.setTextSize(30);
+        textView.setText("i'm parent two");
+        setContentView(textView, new ViewGroup.LayoutParams
+                (ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        L.e("onDestroy");
+    }
+}
